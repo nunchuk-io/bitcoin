63658148d3c8e64e8be0b586db396418a57c3c6f
Andrew Chow <achow101-github@achow101.com>
2019-07-08 15:34:37 -0400

    Store WalletDescriptor in DescriptorScriptPubKeyMan

--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -484,8 +484,17 @@ public:
 
 class DescriptorScriptPubKeyMan : public ScriptPubKeyMan
 {
+private:
+    WalletDescriptor descriptor GUARDED_BY(cs_desc_man);
+
+    using ScriptPubKeyMap = std::map<CScript, int32_t>; // Map of scripts to descriptor range index
+
+    ScriptPubKeyMap m_map_script_pub_keys GUARDED_BY(cs_desc_man);
 public:
-    using ScriptPubKeyMan::ScriptPubKeyMan;
+    DescriptorScriptPubKeyMan(WalletStorage& storage, WalletDescriptor& descriptor)
+        :   ScriptPubKeyMan(storage),
+            descriptor(descriptor)
+        {}
 
     mutable RecursiveMutex cs_desc_man;
 
