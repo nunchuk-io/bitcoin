13e0a258259e0681d7b0cd2adb5549b6532edd3c
Andrew Chow <achow101-github@achow101.com>
2019-07-09 19:34:39 -0400

    Implement several simple functions in DescriptorScriptPubKeyMan
    Implements a bunch of one liners: UpgradeKeyMetadata, IsFirstRun, HavePrivateKeys,
    KeypoolCountExternalKeys, GetKeypoolSize, GetTimeFirstKey, CanGetAddresses,
    RewriteDB

--- src/wallet/scriptpubkeyman.h
+++ src/wallet/scriptpubkeyman.h
@@ -516,8 +516,6 @@ public:
 
     bool HavePrivateKeys() const override;
 
-    void RewriteDB() override;
-
     int64_t GetOldestKeyPoolTime() override;
     size_t KeypoolCountExternalKeys() override;
     unsigned int GetKeyPoolSize() const override;
